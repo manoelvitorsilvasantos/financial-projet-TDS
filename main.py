ac12d271e9b106de0595c2da6676087641a2ea2d
manoel vitor <mvictordossantos.limite@gmail.com>
2022-06-16 13:13:40 -0300

    update!

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ class Main:
 
 	def __init__(self):
 		pass
+
+	def limpar(self):
+		#verifica se o sistema é linux ou windows
+		if system() == 'Linux':
+			sys('cls')
+		elif system() == 'Window':
+			sys('clear')
 	#converte moedas
 	def toCoin(self):
 		value = input(' Digite o valor em Real: ')
@@ -17,44 +24,52 @@ class Main:
 		saida = c.fromNewCoin()
 		print(f'\n\n{value} Reais em {moeda} = {saida}')
 		btn = input('\n\nDigite alguma tecla pra voltar\n')
-		#verifica se o sistema é linux ou windows
-		if system() == 'Linux':
-			sys('cls')
-		elif system() == 'Window':
-			sys('clear')
+		self.limpar()
 	#inicio do programa
 	def init(self):
-		print(
-			'--------- MENU DE OPÇÕES ---------------------\n'
-			'1 - Planejamento de Receita e despesas\n',
-			'2 - Cadastro de Receitas e Despesas realizadas\n',
-			'3 - Visualização comparativa\n',
-			'4 - Visualização de Saldo\n',
-			'5 - Análise de Viabilidade de Investimento\n',
-			'6 - Conversão de Moedas\n',
-			'0 - Fechar aplicativo\n'
-		)
-		opc = int(input(' digite um valor correspondente a opção >>  '))
+		lista_opcoes = [
+			'================= Menu de opções ================',
+			'| 1 - Planejamento de Receita e despesas         |',
+			'| 2 - Cadastro de Receitas e Despesas realizadas |',
+			'| 3 - Visualização comparativa                   |',
+			'| 4 - Visualização de Saldo                      |',
+			'| 5 - Análise de Viabilidade de Investimento     |',
+			'| 6 - Conversão de Moedas                        |',
+			'| 0 - Fechar aplicativo                          |',
+			'=================================================|',
+		]
 
-		if (opc == 0):
-			print(' Fechando programa...')
-			sleep(1)
-			exit(0)
-		elif (opc == 1):
-			plainning()
-		elif (opc == 2):
-			register()
-		elif (opc == 3):
-			viewComparative()
-		elif (opc == 4):
-			viewBalance()
-		elif (opc == 5):
-			analyseViability()
-		elif (opc == 6):
-			self.toCoin()
+		for item in lista_opcoes:
+			print(item)
+
+		opc = input('| digite um valor correspondente a opção >>  ')
+
+		if opc.isdigit() == True:
+			self.limpar()
+
+			opc = int(opc)
+
+			if (opc == 0):
+				print(' Fechando programa...')
+				sleep(1)
+				exit(0)
+			#elif (opc == 1):
+				#plainning()
+			#elif (opc == 2):
+				#register()
+			#elif (opc == 3):
+				#viewComparative()
+			#elif (opc == 4):
+				#viewBalance()
+			#elif (opc == 5):
+				#analyseViability()
+			elif (opc == 6):
+				self.toCoin()
+			else:
+				print(' Opção Inválida!!!\n')
 		else:
-			print(' Opção Inválida!!!\n')
-
+			print('Error, Você precisa digitar um valor númerico!!!')
 #roda o programa em um loop infinito
 while True:
-	Main().init()
+	c = Main()
+	c.init()
